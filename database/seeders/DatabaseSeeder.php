b21802a890d9fc6fc9d3702ed9f6ca81a17332f2
Mohamed <nrookman_27@yahoo.com>
2022-08-28 02:06:39 +0200

    User factory&seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::where('email', '=', 'admin@admin.com')->first();
+        if (!$user) {
+            User::factory()->create([
+                'name' => 'HaMaDa',
+                'email' => 'admin@admin.com',
+                'type' => 'admin',
+            ]);
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(10)->create();
     }
 }
